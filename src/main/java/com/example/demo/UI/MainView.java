31a6020ea658744e988345d0581c99b0b97bd72d
apokalipsa44 <m_lachowicz@wp.pl>
2019-09-06 00:58:21 +0200

    images working

--- src/main/java/com/example/demo/UI/MainView.java
+++ src/main/java/com/example/demo/UI/MainView.java
@@ -1,4 +1,14 @@
 package com.example.demo.UI;
 
-public class MainViewgit add -A {
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("Main-view")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        Image image= new Image("frontend/images/burger.png", "alt burger");
+        add(image);
+    }
 }
